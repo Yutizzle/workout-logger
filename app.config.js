653b070ec88e30557db87772316cfd56ee76a901
Yutizzle <yhlovan@gmail.com>
2022-02-03 15:35:10 -0800

    started expo build process

--- app.config.js
+++ app.config.js
@@ -7,4 +7,10 @@ export default {
     devSupabaseURL: process.env.REACT_APP_SUPABASE_URL,
     devSupabaseAnonKey: process.env.REACT_APP_SUPABASE_ANON_KEY
   },
+  android: {
+    package: "com.yutizzle.workoutlogger"
+  },
+  ios: {
+    bundleIdentifier: "com.yutizzle.workoutlogger"
+  }
 };
