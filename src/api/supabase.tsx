5fe64ce8de6df812582fb73d7aee11d5388948d0
Yutizzle <yhlovan@gmail.com>
2022-02-06 01:33:26 -0800

    cleanup supabase.tsx

--- src/api/supabase.tsx
+++ src/api/supabase.tsx
@@ -1,10 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import Constants from 'expo-constants';
+import 'react-native-url-polyfill/auto';
 
-const supabaseUrl: string = Constants.manifest?.extra?.devSupabaseURL ?? '';
-const supabaseAnonKey: string = Constants.manifest?.extra?.devSupabaseAnonKey ?? '';
+const supabaseUrl = Constants.manifest?.extra?.devSupabaseURL;
+const supabaseAnonKey = Constants.manifest?.extra?.devSupabaseAnonKey;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   localStorage: AsyncStorage as any,
